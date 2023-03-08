4af76e1a9fd0b8766223bdf9380edc66405d3f2a
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2023-03-08 17:12:17 -0500

    V6

--- src/routers/homepage.js
+++ src/routers/homepage.js
@@ -20,7 +20,7 @@ router.get('/homepage',auth,async (req,res)=>{
 	let tags = await Tags.find();
 	///// add tags
 	
-	res.status(200).render("homepage", user)
+	res.status(200).render("homepage", user, tags)
 	
 })
 
@@ -37,27 +37,8 @@ router.get('/homepage/info', auth, async (req, res) =>{
 	let posts = await Post.find();
 	
 	
-	let name;
-	if(posts.length > 1 ){
-		for(let x of posts){
-			name = await User.findOne({_id:x.owner})
-			name = name.info.name.first + " " + name.info.name.last
-			x.name = name
-			
-			obj = {
-				...x,
-				name:name
-			}
-			results.push(obj)
-		}
-	}
-	else{
-		name = await User.findOne({_id:posts.owner})
-		name = name.info.name.first + " " + name.info.name.last
-		posts.name = name
-	}
-	console.log(results)
-	res.status(200).send(results)
+	
+	res.status(200).send(posts)
 })
 
 
