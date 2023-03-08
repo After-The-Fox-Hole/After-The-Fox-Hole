da1a0d953565e3ad85bd89ddf7fa939411380e09
Dereck Rascon <dereckrascon76@gmail.com>
2023-03-08 15:02:25 -0700

    working on edit function

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -4,9 +4,11 @@ const end_button = document.getElementById("end-editing");
 
 edit_button.addEventListener("click", function() {
     edit_form.contentEditable = true;
+    edit_form.style.backgroundColor ="yellow";
 } );
 
 end_button.addEventListener("click", function() {
     edit_form.contentEditable = false;
+    edit_form.style.backgroundColor ="white";
 } )
 
