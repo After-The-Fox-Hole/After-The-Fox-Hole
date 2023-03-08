e1c2507b34dc9e0fea93a14902ed064d4d6280a4
Dereck Rascon <dereckrascon76@gmail.com>
2023-03-08 08:00:29 -0700

    added code for delete button

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,11 +1,12 @@
-const paragraph = document.getElementById("edit");
+const edit_form = document.getElementById("edit");
 const edit_button = document.getElementById("edit-button");
 const end_button = document.getElementById("end-editing");
 
 edit_button.addEventListener("click", function() {
-    paragraph.contentEditable = true;
+    edit_form.contentEditable = true;
 } );
 
 end_button.addEventListener("click", function() {
-    paragraph.contentEditable = false;
+    edit_form.contentEditable = false;
 } )
+
