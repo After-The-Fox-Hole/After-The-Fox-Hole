716eb745fcf1f29a0935ca6f8565172f1dbd256d
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2023-03-16 12:34:26 -0400

    fixed sticky on registration

--- src/routers/user.js
+++ src/routers/user.js
@@ -63,9 +63,10 @@ router.post('/users',async (req,res)=>{
 	}
 	catch (e){
 		let error={
-			error:"Email already exsists"
+			error:"Email already exists"
 		}
-		res.status(200).render("registerUser", ({error}));
+		let attempt = user
+		res.status(200).render("registerUser", ({attempt,error}));
 		return;
 	}
 })
